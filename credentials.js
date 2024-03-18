1a3452c8cf5dc39dee27d5e8626332c0bdbbc9b8
pgin-stayin2020 <68094912+pgin-stayin2020@users.noreply.github.com>
2024-03-18 14:57:44 +0800

    Add files via upload

---
+++ credentials.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAjTzjQaILD-Lrkyw3Hdl2JRXNOXqsV51w",
+    authDomain: "nicer-garlic-app.firebaseapp.com",
+    databaseURL: "https://nicer-garlic-app-default-rtdb.firebaseio.com",
+    projectId: "nicer-garlic-app",
+    storageBucket: "nicer-garlic-app.appspot.com",
+    messagingSenderId: "648624765084",
+    appId: "1:648624765084:web:f7055f8747b0b1c0b1d559"
+};
